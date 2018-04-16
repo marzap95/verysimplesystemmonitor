3c15d4d79a274165932a3496269534f3a6c51dda
marzap95 <mariozappala3@outlook.it>
2018-04-16 16:51:17 +0200

    implemented Ram class

---
+++ ram.cpp
@@ -0,0 +1,35 @@
+class Ram {
+  public:
+    int get_total(){
+      ifstream inputfile;
+      string stringa;
+      int tot;
+      inputfile.open("/proc/meminfo");
+      inputfile >> stringa;
+      inputfile >> tot;
+      inputfile.close();
+      return tot;
+    }
+    int get_used(){
+      ifstream inputfile;
+      string stringa;
+      int libera;
+      int used;
+      int totale = get_total();
+      inputfile.open("/proc/meminfo");
+      getline(inputfile, stringa);
+      getline(inputfile, stringa);
+      inputfile >> stringa;
+      inputfile >> libera;
+      used = totale - libera;
+      inputfile.close();
+      return used;
+    }
+    int get_used_percent(){
+      float percent;
+      int used = get_used();
+      int total = get_total();
+      percent = (float)total *100 / (float)used;
+      return (int)percent;
+    }
+};
